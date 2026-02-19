bccd4fe3aeec4d36efbe8a91484abf8c7651d598
Ian Leckie <ianleckie@hey.com>
2026-02-18 23:09:20 -0600

    icons and JS work

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,22 +1,29 @@
 /**
- * ProbablyEnough.dev site object
+ * ProbablyEnough.dev main object
  */
 probablyEnough = {
 
     addPrompts() {
+        const promptStart = '<div class="prompt">ian@probablyenough:~$<span class="cmd"';
+        const promptEnd = '</span></div>';
+        
+        // foreach .line, add prompt based on data-cmd-
+            var promptCmd = '';
+            // CMDs:
+            // '> echo {{echo}}'
+            // ' id="cursor"> █'
 
+            var prompt = promptStart + promptCmd + promptEnd;
     },
 
     blinkCursor() {
-
-    },
-
-    blinkElement( element ) {
-
+        // for .cursor, blink the text
     },
 
     initBtns() {
-
+        // foreach .btn
+            // get first link url, block link action
+            // add onclick = url, onhover = show in status
     },
 
     init() {
